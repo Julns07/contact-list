fbd5cf6c3f54bc15f3fab021b80b046a146ebbca
Kyler Wilson <kyleraw2001@gmail.com>
2020-02-19 16:36:31 -0800

    styled some and made code prettier

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './Home'
-// import data from '../names.json'
 import contacts from './List.js'
-// import single from './SingleContact.js'
 
 
 function App() {
